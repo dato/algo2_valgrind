2bab8a2364fb676cda634644031623095d84d1de
Martin Buchwald <martin.buchwald@mulesoft.com>
2019-01-12 15:56:40 -0300

    Cambios ejercicio 2

--- nivel-01/ejercicio-02/ej2.c
+++ nivel-01/ejercicio-02/ej2.c
@@ -1,14 +1,40 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <stddef.h>
 #define TAM 10
 
-int main(void) {
-	int arr[TAM];
-	for (size_t i = 0; i <= TAM; i++) {
-		arr[i] = (int) i;
+void rellenar_arreglo(int* arr, size_t n) {
+	for (size_t i = 1; i <= n; i++) {
+		arr[i] = i;
 	}
-	for (size_t i = 0; i <= TAM; i++) {
+}
+
+void imprimir_arreglo(int* arr, size_t n) {
+	for (size_t i = 1; i <= n; i++) {
 		printf("%d\n", arr[i]);
+	}
+}
+
+bool esta_ordenado(int* arr, size_t n) {
+	for (size_t i = 0; i < n; i++) {
+		if (arr[i] > arr[i+1]) {
+			return false;
+		}
+	}
+	return true;
+}
+
+
+int main(void) {
+	int* arreglo = malloc(sizeof(int) * TAM);
+	rellenar_arreglo(arr, TAM);
+	imprimir_arreglo(arr, TAM);
+	if (esta_ordenado(arr, TAM)) {
+		printf("El arreglo esta ordenado\n");
+	} else {
+		printf("El arreglo no esta ordenado\n");
 	}
 	return 0;
 }
+
+
